649280400b44f111e530102b369aea9c0b4ce865
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-06-06 16:49:38 +0200

    reduce number of images variations

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,16 @@ module.exports = defineConfig({
   pluginOptions: {
     vuetify: {},
   },
+  pwa: {
+    name: 'Ging',
+    themeColor: '#fb923c',
+    iconPaths: {
+      faviconSVG: 'images/logo.svg',
+      favicon32: 'images/logo-32.png',
+      favicon16: 'images/logo-16.png',
+      appleTouchIcon: 'images/logo-512-maskable.png',
+      maskIcon: 'images/logo.svg',
+      msTileImage: 'images/logo-512.png',
+    },
+  },
 })
