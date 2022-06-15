666d710963ed2e0047a01cdc1ce1b51396c31aee
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-06-16 00:12:13 +0200

    simpler step add via title & duration

--- src/components/add-step.vue
+++ src/components/add-step.vue
@@ -3,23 +3,16 @@
     <v-card>
       <v-container>
         <v-col class="min-w-[20rem]">
-          <div class="text-h5 mb-12">New step</div>
-          <v-form ref="form" v-model="valid">
-            <v-text-field v-model="title" :rules="requiredRules" label="Title" autofocus required />
-            <v-text-field v-model="delay" type="number" :step="1" :min="0" :max="100" :suffix="delayType" :rules="requiredRules" label="Delay"
-                          required />
-            <v-slider v-model="delay" :step="1" :min="0" :max="100" color="orange" />
-            <div class="h-2"></div>
-            <v-radio-group v-model="delayType" inline>
-              <v-radio label="Days" value="days" />
-              <v-radio label="Hours" value="hours" />
-            </v-radio-group>
+          <div class="text-h5 mb-4">New step</div>
+          <v-form ref="form" v-model="valid" @submit="submit">
+            <v-text-field v-model="title" :rules="requiredRules" label="Step title, time" autofocus required
+                          hint="Like &ldquo;Get some milk, 1 hour&rdquo; or &ldquo;Go to Japan, 3 weeks&rdquo;" />
           </v-form>
         </v-col>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn variant="plain" @click="close()">Cancel</v-btn>
-          <v-btn :disabled="!valid" variant="contained" color="primary" @click="submit()">Add</v-btn>
+          <v-btn variant="plain" @click="close">Cancel</v-btn>
+          <v-btn :disabled="!valid" variant="contained" color="primary" @click="submit">Add</v-btn>
         </v-card-actions>
       </v-container>
     </v-card>
@@ -30,6 +23,7 @@
 import { useStore } from '@/store'
 import { Step } from '@/types'
 import { requiredRules } from '@/utils/form-rules'
+import { stringToStepData } from '@/utils/step'
 import colors from 'tailwindcss/colors'
 import { defineComponent } from 'vue'
 
@@ -45,8 +39,6 @@ export default defineComponent({
     open: false,
     valid: false,
     title: '',
-    delay: 0,
-    delayType: 'days',
     requiredRules,
   }),
   computed: {
@@ -66,32 +58,15 @@ export default defineComponent({
     close () {
       this.open = false
     },
-    submit () {
-      console.log('submit step')
+    submit (event: Event) {
+      event.preventDefault()
       const store = useStore()
-      const step = new Step({
-        title: this.title,
-      })
-      if (this.delayType === 'days') step.days = this.delay
-      else step.hours = this.delay
+      const step = new Step(stringToStepData(this.title))
+      console.log('submit, adding step', step)
       store.addStep(step)
       this.title = ''
-      this.delay = 0
-      this.delayType = 'days'
       this.close()
     },
   },
 })
 </script>
-
-
-<style>
-.v-form>.v-input+.v-input .v-input__details {
-  @apply mb-0;
-}
-
-.v-radio-group .v-input__details,
-.v-slider .v-input__details {
-  @apply hidden;
-}
-</style>
