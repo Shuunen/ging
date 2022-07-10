c025dad0763c3958987b5609222953f45b1c14c7
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-07-10 19:31:51 +0200

    enhance global render

--- src/components/app-navbar.vue
+++ src/components/app-navbar.vue
@@ -3,8 +3,8 @@
     <v-container>
       <v-row class="items-center pt-0.5">
         <div class="flex flex-row gap-2 pt-1">
-          <h1 class="text-h4 sm:hidden ml-2">G</h1>
-          <h1 class="text-h4 sm:block hidden ml-2">GING</h1>
+          <h1 class="text-h4 sm:hidden font-weight-thin ml-2">G</h1>
+          <h1 class="text-h4 sm:block font-weight-thin hidden ml-2">GING</h1>
           <v-icon v-if="!activeProject" class="text-h4" color="secondary" icon="mdi-chevron-triple-right" />
         </div>
         <div class="ml-auto"></div>
