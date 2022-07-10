1edd4421bd48f807a9ed403cc4e7315213ca5b36
Romain Racamier-Lafon <romain.racamier@gmail.com>
2022-07-10 11:44:16 +0200

    show footer on debug mode

---
+++ src/components/app-footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-show="debugMode" class="flex flex-col items-center justify-center pb-1 mx-auto">
+    <app-debug />
+    <span class="text-body-1 mx-auto">GING &copy; {{ new Date().getFullYear() }}</span>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { useStore } from '@/store'
+import { mapState } from 'pinia'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  computed: {
+    ...mapState(useStore, ['debugMode']),
+  },
+})
+</script>
