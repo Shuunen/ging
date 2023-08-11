e5b42a31f10462d34969f8625c6da77c22d444e6
Romain Racamier-Lafon <romain.racamier@gmail.com>
2023-08-11 12:40:55 +0200

    enhance mobile navbar spacings

--- src/components/app-navbar.vue
+++ src/components/app-navbar.vue
@@ -7,7 +7,7 @@ import { activeProject, store } from '@/store'
     <v-container>
       <v-row class="items-center pt-0.5">
         <div class="flex flex-row gap-2 pt-1">
-          <h1 class="ml-2 text-4xl font-thin sm:hidden">G</h1>
+          <h1 class="ml-6 text-4xl font-thin sm:hidden">G</h1>
           <h1 class="ml-2 hidden text-4xl font-thin sm:block">GING</h1>
           <v-icon v-if="!activeProject" class="text-4xl" color="secondary" icon="mdi-chevron-triple-right" />
           <v-progress-circular v-show="store.isLoading" class="mt-0.5" color="primary" indeterminate></v-progress-circular>
@@ -20,7 +20,7 @@ import { activeProject, store } from '@/store'
         <div class="sm:hidden">
           <app-actions />
         </div>
-        <div class="ml-1">
+        <div class="ml-1 mr-6 md:mr-0">
           <app-login-button />
           <app-about-button />
         </div>
